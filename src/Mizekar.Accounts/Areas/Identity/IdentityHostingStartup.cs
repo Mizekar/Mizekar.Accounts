6dbee403a14098a27be35285c6b7e37e67d453be
AbolfazlRajabpour <Abolfazl.rajabpour@gmail.com>
2018-12-31 16:00:40 +0330

    Add identity default ui

---
+++ src/Mizekar.Accounts/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Mizekar.Accounts.Data;
+using Mizekar.Accounts.Data.Entities;
+
+[assembly: HostingStartup(typeof(Mizekar.Accounts.Areas.Identity.IdentityHostingStartup))]
+namespace Mizekar.Accounts.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
