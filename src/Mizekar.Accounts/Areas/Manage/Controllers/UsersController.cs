5e8c14dad296410046cac2a0f88c2d26507352a0
AbolfazlRajabpour <Abolfazl.rajabpour@gmail.com>
2019-02-26 15:15:05 +0330

    manage users

---
+++ src/Mizekar.Accounts/Areas/Manage/Controllers/UsersController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mizekar.Accounts.Data;
+using Mizekar.Accounts.Data.Entities;
+
+namespace Mizekar.Accounts.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class UsersController : Controller
+    {
+        private readonly AccountsDbContext _context;
+
+        public UsersController(AccountsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Manage/Users
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Manage/Users/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // GET: Manage/Users/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.Users.FindAsync(id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: Manage/Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("FirstName,LastName,Id")] ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await _context.Users.FindAsync(id);
+                    user.FirstName = applicationUser.FirstName;
+                    user.LastName = applicationUser.LastName;
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationUserExists(applicationUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(applicationUser);
+        }
+
+        private bool ApplicationUserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
